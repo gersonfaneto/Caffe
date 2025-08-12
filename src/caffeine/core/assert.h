f82946477538bb86e2462ad3159ad45eab18b9b7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-08-12 17:02:41 -0300

    refactor(core): move assertion macros into separate file

---
+++ src/caffeine/core/assert.h
@@ -0,0 +1,26 @@
+#ifndef CAFFEINE_ASSERT_H_
+#define CAFFEINE_ASSERT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define __ASSERT__(condition, fmt, ...)          \
+  do                                             \
+  {                                              \
+    if (!(condition))                            \
+    {                                            \
+      fprintf(stderr, fmt "\n", ##__VA_ARGS__);  \
+      exit(1);                                   \
+    }                                            \
+  }                                              \
+  while (0);                                     \
+
+#define __TODO__() \
+  __ASSERT__(0, "%s:%d: [TODO]: Something is missing...", __FILE__, __LINE__)
+
+#define __UNREACHABLE__() \
+  __ASSERT__(0, "%s:%d: [PANIC]: How did we get here...?", __FILE__, __LINE__)
+
+#endif // ifndef CAFFEINE_ASSERT_H_
+
+// vim:sw=2:ts=2:et:
