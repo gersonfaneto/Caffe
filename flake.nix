ac1d3653b3c7ad20d3d957a2937b30673eaaebd1
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2025-08-26 12:28:16 -0300

    build(flake): setup development environment using Nix Flakes

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Development Enviroment for Caffè";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          # C/C++ Tools
+          clang-tools
+
+          # YAML Tools
+          yaml-language-server
+        ];
+      };
+  };
+}
